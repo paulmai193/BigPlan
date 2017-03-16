5bf0be15188b7eb6288d94497110d1f98d750845
Paul Mai <dai.maithanh@gmail.com>
2017-03-16 11:01:09 +0700

    1.0.10: fix bug add 2 home screen

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,10 +5,10 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		// set a web app capable website
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="mobile-web-app-capable" content="yes">
 		<link rel="manifest" href="/manifest.json">
+		// set a web app capable website
+		<!--<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="mobile-web-app-capable" content="yes">-->
 		<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.min.css">
 		<link rel="stylesheet" href="//code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
